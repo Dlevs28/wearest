660fd8dbc40461a28a052e3885ed564f333025cc
Dana Leventhal <soleteamNY@gmail.com>
2015-03-16 16:39:18 -0400

    made changes

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    invite_code = params[:invite_code]
+    if invite_code == 'RSVP123'
+      super
+    else
+      redirect_to root_path, :notice => 'Your invitation code is not valid'
+    end
+  end
+
+end
